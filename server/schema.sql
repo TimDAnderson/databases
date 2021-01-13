ca6986133694b25cacd3323fe85650dbe51edb83
Tim Anderson <tdanderson805@gmail.com>
2021-01-12 20:18:42 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,10 +3,18 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  ID integer AUTO_INCREMENT,
+  createdAt VARCHAR(30),
+  objectID VARCHAR(30),
+  roomname text,
+  messageText text,
+  updatedAt VARCHAR(30),
+  username text,
+  PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
+
 
 
 
